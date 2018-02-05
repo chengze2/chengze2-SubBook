198026eabfdeb5a5e946e706d6ee843230f3d23a
chengze2 <chengze2@ualberta.ca>
2018-02-05 14:35:43 -0700

    fix return button error, add dollar sign, add new apk files

---
+++ app/build/generated/source/buildConfig/release/ca/ualberta/cs/chengze2_subbook/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ca.ualberta.cs.chengze2_subbook;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ca.ualberta.cs.chengze2_subbook";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
